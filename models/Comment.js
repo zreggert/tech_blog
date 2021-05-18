07cc87518734e44c565776df8a834eb7c2e5dbfb
elgalloloco <zreggert@gmail.com>
2021-05-18 16:04:03 -0400

    added comment model

---
+++ models/Comment.js
@@ -0,0 +1,39 @@
+const { Model, Datatypes} = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Comment extends Model {}
+
+Comment.init(
+    {
+        id: {
+            type: Datatypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        date_posted: {
+            type: Datatypes.DATE,
+            allowNull: false,
+            defaultValue: Datatypes.NOW,
+        },
+        post_content: {
+            type: Datatypes.STRING
+        },
+        user_id: {
+            type: Datatypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id',
+            },
+        },
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'user',
+    }
+);
+
+module.exports = Comment;
