ea963d41fad773423aa4141fbc64e59ee709825e
elgalloloco <zreggert@gmail.com>
2021-05-18 22:46:27 -0400

    added login and signup handlebars and routes to access login handlebar from home

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -4,4 +4,12 @@ router.get('/', (req, res) => {
     res.render('home');
 });
 
+router.get('/login', (req, res) => {
+    res.render('login');
+});
+
+router.get('/signup', (req, res) => {
+    res.render('signup');
+});
+
 module.exports = router;
