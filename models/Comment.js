12204b3430600ead60b9a1711371c9f36b263241
elgalloloco <zreggert@gmail.com>
2021-05-27 20:14:02 -0400

    comment functions are partly functional. still need to figure out how to get the content of the comments to display

--- models/Comment.js
+++ models/Comment.js
@@ -5,34 +5,34 @@ class Comment extends Model {}
 
 Comment.init(
     {
-        id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            primaryKey: true,
-            autoIncrement: true
-        },
-        date_posted: {
-            type: DataTypes.DATE,
-            allowNull: false,
-            defaultValue: DataTypes.NOW,
-        },
+        // id: {
+        //     type: DataTypes.INTEGER,
+        //     allowNull: false,
+        //     primaryKey: true,
+        //     autoIncrement: true
+        // },
+        // date_posted: {
+        //     type: DataTypes.DATE,
+        //     allowNull: false,
+        //     defaultValue: DataTypes.NOW,
+        //},
         comment_content: {
             type: DataTypes.STRING
         },
-        user_id: {
-            type: DataTypes.INTEGER,
-            references: {
-                model: 'user',
-                key: 'id',
-            },
-        },
+        // user_id: {
+        //     type: DataTypes.INTEGER,
+        //     references: {
+        //         model: 'user',
+        //         key: 'id',
+        //     },
+        // },
     },
     {
         sequelize,
-        timestamps: false,
-        freezeTableName: true,
-        underscored: true,
-        modelName: 'comment',
+        // timestamps: false,
+        // freezeTableName: true,
+        // underscored: true,
+        // modelName: 'comment',
     }
 );
 
---
+++ public/js/comment.js
@@ -0,0 +1,24 @@
+const createComment = async (event) => {
+    event.preventDefault();
+
+    const postId = document.querySelector('input[name="post-id"]').value;
+    const body = document.querySelector('textarea[name="comment-input"]').value;
+
+    if (body) {
+        await fetch('/api/comment', {
+            method: 'POST',
+            body: JSON.stringify({ 
+                postId,
+                body, 
+            }),
+            headers: {
+                'Content-Type': 'application/json' 
+            },
+        });
+        document.location.reload();
+    }
+};
+
+document
+    .querySelector('#new-comment-form')
+    .addEventListener('submit', createComment);
